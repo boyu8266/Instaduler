effbe0701f0a745d676898d1f1cd46cb6ef8e8c8
boyu8266 <boryuh8266@gmail.com>
2023-10-05 00:03:44 +0800

    feat: add login functionality to command line interface
    - Added a new file `instaduler/__main__.py` with 34 lines of code
    - Created a function `login` that logs in a user with a given username and password
    - Added a `main` function that handles command line arguments and calls the appropriate function based on the specified action
    - Added a subcommand `login` to the command line interface
    - Added command line arguments `-u` and `-p` to specify the username and password respectively
    - Dump the settings to a file `config/session.json` after logging in
    - Print a message with the username and password after logging in
    - If no action is specified, print the help message
    
    Signed-off-by: boyu8266 <boryuh8266@gmail.com>

---
+++ instaduler/__main__.py
@@ -0,0 +1,34 @@
+import argparse
+
+from instagrapi import Client
+
+
+def login(username, password):
+    client = Client()
+    client.login(username=username, password=password)
+    client.dump_settings("config/session.json")
+    print(f"Logging in with user: {username}, password: {password}")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Login operations")
+    subparsers = parser.add_subparsers(dest="action", help="Action to perform")
+
+    # Subcommand: login
+    login_parser = subparsers.add_parser(
+        "login", help="Perform login operation")
+    login_parser.add_argument(
+        "-u", "--user", required=True, help="Specify the username")
+    login_parser.add_argument(
+        "-p", "--psw", required=True, help="Specify the password")
+
+    args = parser.parse_args()
+
+    if args.action == "login":
+        login(args.user, args.psw)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
