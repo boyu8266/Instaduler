96655f29c863adeb0616bf33a809d2c0274989e0
boyu8266 <boryuh8266@gmail.com>
2023-10-05 12:59:12 +0800

    feat: add new constants and attribute to Post class
    - Add two new constants `ALBUM` and `REELS`
    - Add a new attribute `classification` to the `Post` class
    
    Signed-off-by: boyu8266 <boryuh8266@gmail.com>

--- instaduler/model/post.py
+++ instaduler/model/post.py
@@ -2,8 +2,12 @@ from typing import List
 
 from pydantic import BaseModel
 
+ALBUM = 'ALBUM'
+REELS = 'REELS'
+
 
 class Post(BaseModel):
+    classification: str = ALBUM
     files: List[str]
     caption: str
 
